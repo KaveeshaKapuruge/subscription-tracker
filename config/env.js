b000fc860dfd95b10f4602d327de15ec799567f1
Kaveesha Kapuruge <kaveeshakapuruge@gmail.com>
2025-08-08 11:36:26 +0530

    INITIAL COMMIT

---
+++ config/env.js
@@ -0,0 +1,7 @@
+import { config } from "dotenv";
+
+// Set the correct path depending on NODE_ENV or default to 'development'
+const env = process.env.NODE_ENV || "development";
+config({ path: `.env.${env}.local` });
+
+export const { PORT, NODE_ENV } = process.env;
